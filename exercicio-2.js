11d9652e29dc7df45e44eec33f1bf9fd01e90ea6
Davi Santos <102557963+DaviSantosDEV@users.noreply.github.com>
2022-05-12 15:04:20 -0300

    Add files via upload

---
+++ exercicio-2.js
@@ -0,0 +1,25 @@
+//let NASC = parseInt(prompt('VEJA SE VOCÊ PODE VOTAR '));
+
+//if( nasc <= 2006 ){
+   // alert(`Voce pode Votar `)}
+//else{
+    //alert('Você Não pode Votar')}
+
+
+//Atividade 2
+
+//let val = parseInt(prompt('SENHA DO USUARIO'));
+
+//if( val == 1234 ){
+    //alert(`Senha valida `)}
+//else{
+    //alert('Senha Invalida')}
+
+
+//Atividade 3
+
+
+if (turno == 'm'){ alert('bom dia') }
+else if(turno == 'v'){ alert('boa tarde') }
+else if(turno == 'n'){ alert('boa noite') }
+else{ alert('valor infalido') }
